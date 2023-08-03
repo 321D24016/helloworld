dcc8b28a7bb87c04825a6e92e1d8f30686b49117
321D24016 <140032768+321D24016@users.noreply.github.com>
2023-08-03 22:34:34 +0900

    add

--- member.py
+++ member.py
@@ -1,6 +1,3 @@
-ver = "0.2"
-ver = "0.17"
-
 ver = "0.3"
 
 class Member:
@@ -20,6 +17,7 @@ newmember = Member("江頭2:50", "エガちゃんです！")
 mlist.append(newmember)
 
 ### 以下に自分を追加する ###
+
 newmember = Member("小島匠人", "です！")
 mlist.append(newmember)
 
